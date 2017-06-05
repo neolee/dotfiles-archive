1af7b57957ed12fd45b697d8bf6eb29712c7fdaf
Neo Lee <neo.lee@gmail.com>
2017-06-05 11:37:34 +0800

    Add commonly used plugins

--- profiles.clj
+++ profiles.clj
@@ -3,10 +3,10 @@
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [criterium "0.4.4"]]
         :plugins [
-                  ; [cider/cider-nrepl "0.13.0"]
                   [cider/cider-nrepl "0.14.0"]
-                  ; [refactor-nrepl "2.2.0"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]
+                  [lein-ancient "0.6.10"]
+                  [lein-clean-m2 "0.1.2"]
                   [lein-marginalia "0.9.0"]
                   ]}
  }
