a82e8416cffe90844d095fb08afe0690a14ad71b
Neo Lee <neo.lee@gmail.com>
2017-06-05 16:49:22 +0800

    Remove buggy lein-clean-m2

--- profiles.clj
+++ profiles.clj
@@ -6,7 +6,6 @@
                   [cider/cider-nrepl "0.14.0"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]
                   [lein-ancient "0.6.10"]
-                  [lein-clean-m2 "0.1.2"]
                   [lein-marginalia "0.9.0"]
                   ]}
  }
