037deff289db70141846c5cb85d0589ab6d27a37
Neo Lee <neo.lee@gmail.com>
2018-01-07 13:25:37 +0800

    Regular updates

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,10 @@
 {:live-repl {:repl-options {:port 4555}}
  :user {:aliases {"live" ["with-profile" "default,live-repl" "repl"]}
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
-                       [criterium "0.4.4"]]
+        :dependencies [[alembic "0.3.2"]
+                       [criterium "0.4.4"]
+                       [org.clojure/tools.nrepl "0.2.12"]]
         :plugins [
-                  [cider/cider-nrepl "0.16.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.17.0-SNAPSHOT"]
                   [refactor-nrepl "2.4.0-SNAPSHOT"]
                   [clj-http "3.7.0"]
                   [lein-ancient "0.6.12"]
