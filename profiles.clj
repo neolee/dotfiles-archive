7d7b7456592b3803d6b53a3de2a7925bf068adab
Neo Lee <neo.lee@gmail.com>
2018-06-11 15:52:48 +0800

    Sync recent changes

--- profiles.clj
+++ profiles.clj
@@ -4,10 +4,10 @@
                        [criterium "0.4.4"]
                        [org.clojure/tools.nrepl "0.2.12"]]
         :plugins [
-                  [cider/cider-nrepl "0.17.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.18.0-SNAPSHOT"]
                   [refactor-nrepl "2.4.0-SNAPSHOT"]
-                  [clj-http "3.7.0"]
-                  [lein-ancient "0.6.12"]
+                  [clj-http "3.9.0"]
+                  [lein-ancient "0.6.15"]
                   [lein-marginalia "0.9.0"]
                   ]}
  }
