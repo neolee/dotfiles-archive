ccd7f29cf502c2cbe6d59df010b6c16070c70da2
Neo Lee <neo.lee@gmail.com>
2018-08-22 18:03:28 +0800

    Update assets

--- profiles.clj
+++ profiles.clj
@@ -4,7 +4,7 @@
                        [criterium "0.4.4"]
                        [org.clojure/tools.nrepl "0.2.12"]]
         :plugins [
-                  [cider/cider-nrepl "0.18.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.18.0"]
                   [refactor-nrepl "2.4.0-SNAPSHOT"]
                   [clj-http "3.9.0"]
                   [lein-ancient "0.6.15"]
