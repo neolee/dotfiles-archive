33807b48230e501a9f49686de5a4c280e63ed82a
Neo Lee <neo.lee@gmail.com>
2018-08-31 17:55:31 +0800

    Update asset

--- profiles.clj
+++ profiles.clj
@@ -5,7 +5,7 @@
                        [org.clojure/tools.nrepl "0.2.12"]]
         :plugins [
                   [cider/cider-nrepl "0.18.0"]
-                  [refactor-nrepl "2.4.0-SNAPSHOT"]
+                  [refactor-nrepl "2.4.0"]
                   [clj-http "3.9.0"]
                   [lein-ancient "0.6.15"]
                   [lein-marginalia "0.9.0"]
