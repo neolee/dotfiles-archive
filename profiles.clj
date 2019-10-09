27fe80984b40645cdbbf42c1e3784d74905be93a
Neo Lee <neo.lee@gmail.com>
2019-10-09 20:52:05 +0800

    Update assets

--- profiles.clj
+++ profiles.clj
@@ -1,13 +1,13 @@
 {:live-repl {:repl-options {:port 4555}}
  :user {:aliases {"live" ["with-profile" "default,live-repl" "repl"]}
-        :dependencies [[alembic "0.3.2"]
+        :dependencies [[clojure-emacs/alembic "0.3.3"]
                        [criterium "0.4.5"]
                        [org.clojure/tools.nrepl "0.2.13"]]
         :middleware [cider-nrepl.plugin/middleware
                      refactor-nrepl.plugin/middleware
                      ]
         :plugins [
-                  [cider/cider-nrepl "0.22.3"]
+                  [cider/cider-nrepl "0.22.4"]
                   [refactor-nrepl "2.5.0-SNAPSHOT"]
                   [clj-http "3.10.0"]
                   [lein-ancient "0.6.15"]
