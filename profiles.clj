d11a83d73ff01feb6f28cd36392e6c44fdbf922d
Neo Lee <neo.lee@gmail.com>
2020-02-05 21:43:17 +0800

    Add GraalVM native image related profiles

--- profiles.clj
+++ profiles.clj
@@ -1,16 +1,24 @@
 {:live-repl {:repl-options {:port 4555}}
- :user {:aliases {"live" ["with-profile" "default,live-repl" "repl"]}
+ :uberjar {:aot :all}
+ :user {:aliases {"live" ["with-profile" "default,live-repl" "repl"]
+                  "native" ["shell"
+                            "native-image" "--report-unsupported-elements-at-runtime"
+                            "--initialize-at-build-time"
+                            "-jar" "./target/${:uberjar-name:-${:name}-${:version}-standalone.jar}"
+                            "-H:Name=./target/${:name}"]
+                  }
         :dependencies [[clojure-emacs/alembic "0.3.3"]
                        [criterium "0.4.5"]
                        [org.clojure/tools.nrepl "0.2.13"]]
         :middleware [cider-nrepl.plugin/middleware
-                     refactor-nrepl.plugin/middleware
-                     ]
-        :plugins [
-                  [cider/cider-nrepl "0.23.0-SNAPSHOT"]
+                     refactor-nrepl.plugin/middleware]
+        :plugins [[cider/cider-nrepl "0.23.0-SNAPSHOT"]
                   [refactor-nrepl "2.5.0-SNAPSHOT"]
                   [clj-http "3.10.0"]
                   [lein-ancient "0.6.15"]
                   [lein-marginalia "0.9.1"]
-                  ]}
+                  [lein-shell "0.5.0"]
+                  ]
+        }
  }
+
